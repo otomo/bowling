0670c8704224261231cc7c070d0eb7ac394c7e55
otomo <the50mile@gmail.com>
2009-12-02 16:09:39 +0900

    refactor Bowling::Game#score
    extract Game#roll_at and Game#two_rolls_from from Game#score.

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -13,21 +13,30 @@ module Bowling
 			score = 0
 			roll_idx = 0
 			10.times do
-				if @rolls[roll_idx] == 10 #strike
-					score += @rolls[roll_idx] #strike
-					score += @rolls[roll_idx + 1] + @rolls[roll_idx + 2] #bonus
+				if roll_at(roll_idx) == 10 #strike
+					score += roll_at(roll_idx) #strike
+					score += two_rolls_from(roll_idx+1) #strike_bonus
 					roll_idx += 1
-				elsif (@rolls[roll_idx] + @rolls[roll_idx + 1]) == 10 #spare
-					score += @rolls[roll_idx] + @rolls[roll_idx + 1] #spare
-					score += @rolls[roll_idx + 2] #bonus
+				elsif two_rolls_from(roll_idx) == 10 #spare
+					score += two_rolls_from(roll_idx) #spare
+					score += roll_at(roll_idx+2) #spare_bonus
 					roll_idx += 2
 				else
-					score += @rolls[roll_idx] + @rolls[roll_idx + 1]
+					score += two_rolls_from(roll_idx)
 					roll_idx += 2
 				end
 			end
 			score
 		end
+
+		private
+			def roll_at(idx)
+				@rolls[idx]
+			end
+
+			def two_rolls_from(idx)
+				@rolls[idx] + @rolls[idx+1]
+			end
 
 	end
 end
